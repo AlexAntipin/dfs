e9d90d291703085f4b6a7c3d9768130f4bff1444
AlexAntipin <antipin-it@mail.ru>
2022-12-31 05:18:39 +0400

    Запилил dfs

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 import re
-# with open('Graph.txt', 'r') as graph:
 
 
+
+
+
+class Graph:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.visited = []
+
+    def print_graph(self):
+        print(self.matrix)
+
+    def dfs(self, v, visited):
+        v -= 1
+        def dfs_help(v, visited):
+            visited.append(v)
+            print(visited)
+            for i in range(len(matrix[v])):
+                if matrix[v][i] == 1 and i not in visited:
+                    dfs_help(i, visited)
+            self.visited = visited
+        dfs_help(v, visited)
+
+
+with open('Graph.txt', 'r') as graph:
+    matrix = []
+    for i in graph.readlines():
+        matrix.append(list(map(int,(i[:-1].split()))))
+
+    print(matrix)
+
+a = Graph(matrix)
+a.print_graph()
+a.dfs(1,[])
 
 print('Hello world')
 
